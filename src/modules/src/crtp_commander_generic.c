30ca1982863903610b72dbaba1a4570084433484
Wolfgang Hoenig <hoenig@tu-berlin.de>
2025-03-26 12:11:05 +0100

    fix build error

--- src/modules/src/crtp_commander_generic.c
+++ src/modules/src/crtp_commander_generic.c
@@ -436,6 +436,7 @@ static void fullStateDecoder(setpoint_t *setpoint, uint8_t type, const void *dat
   setpoint->acceleration.x = (values->a ## x) / 1000.0f; \
   setpoint->jerk.x = 0.0f; \
   setpoint->snap.x = 0.0f; \
+
   UNPACK(x)
   UNPACK(y)
   UNPACK(z)
